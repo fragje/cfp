4e0bce4c9a608bef24ab588c0841847f3dfff170
Frank Gjertsen <fragje@Franks-Mac-Studio.local>
2023-03-03 15:36:55 +0100

    Fix typo

--- src/routes/kv/+server.ts
+++ src/routes/kv/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async (context) => {
-  const countValue = await context.env.CVF.get('count');
+  const countValue = await context.env.CVP.get('count');
 	return new Response(`Hello ${countValue}`);
 };
